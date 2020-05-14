de76a86c9dfa42a29bba0575113f0f2536f3402f
mcmashi <advancedcreator7@gmail.com>
2020-05-14 17:30:02 +0900

    ・追加 シーン：テスト 画像データ：背景、プレイヤー スクリプト：プレイヤー、スクロール ・概要 プレイヤーの移動と背景のスクロール処理の追加

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+// 移動スピード
+    public float speed = 5;
+
+    // 移動する向き
+    private Vector2 dir;
+
+    // 画面の左・右・上・下の境界の位置
+    private float boundL;
+    private float boundR;
+    private float boundT;
+    private float boundB;
+
+    private new Rigidbody2D rigidbody2D;
+
+    private void Start()
+    {
+        this.rigidbody2D = this.GetComponent<Rigidbody2D>();
+
+        // あらかじめ、画面上下左右の縁がワールド空間上でどこに位置するか調べておく
+        var mainCamera = Camera.main;
+        var positionZ = this.transform.position.z;
+        var topRight = mainCamera.ViewportToWorldPoint(new Vector3(1.0f, 1.0f, positionZ));
+        var bottomLeft = mainCamera.ViewportToWorldPoint(new Vector3(0.0f, 0.0f, positionZ));
+        this.boundL = bottomLeft.x + 0.3f;
+        this.boundR = topRight.x - 0.3f;
+        this.boundT = topRight.y - 0.3f;
+        this.boundB = bottomLeft.y + 0.3f;
+    }
+
+    private void Update()
+    {
+        // 右・左
+        var x = Input.GetAxisRaw("Horizontal");
+
+        // 上・下
+        var y = Input.GetAxisRaw("Vertical");
+
+        this.dir = new Vector2(x, y);
+    }
+
+    private void FixedUpdate()
+    {
+        // 現在の位置
+        var position = this.rigidbody2D.position;
+
+        // 範囲制限を加える前の速度
+        var velocity = this.dir * this.speed;
+
+        // 今回の物理状態更新での移動量
+        var deltaPosition = velocity * Time.deltaTime;
+
+        // 範囲制限を加えなかった場合の、予想される移動先
+        var nextPosition = position + deltaPosition;
+
+        // 予想移動先の座標をクランプする
+        var clampedNextPosition = new Vector2(
+            Mathf.Clamp(nextPosition.x, this.boundL, this.boundR),
+            Mathf.Clamp(nextPosition.y, this.boundB, this.boundT));
+
+        // クランプ後の移動先座標から移動量を求める
+        var clampedDeltaPosition = clampedNextPosition - position;
+
+        // クランプ後の移動量から速度を求める
+        var clampedVelocity = clampedDeltaPosition / Time.deltaTime;
+
+        // クランプ後の速度を代入
+        this.rigidbody2D.velocity = clampedVelocity;
+    }
+}
+
---
+++ Assets/Script/Scroll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate( 0, -0.1f, 0);
+        if (transform.position.y < -10.4f)
+        {
+            transform.position = new Vector3( 0, 0, 0);
+        }
+    }
+}
