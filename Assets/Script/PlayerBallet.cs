b578e40440b4c2c35855ad323306cb7d401cb440
mcmashi <advancedcreator7@gmail.com>
2020-05-15 17:46:18 +0900

    追加忘れ

---
+++ Assets/Script/PlayerBallet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBallet : MonoBehaviour {
+
+    //移動スピード
+    public float speed = 7.0f;
+
+    //進む方向
+    private Vector2 dir;
+
+    private new Rigidbody2D rigidbody2D;
+
+	// Use this for initialization
+	void Start () {
+        this.rigidbody2D = this.GetComponent<Rigidbody2D>();
+        this.dir = new Vector2( 0, 1.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.rigidbody2D.velocity = dir * speed;
+
+        //画面外に出ると消える。
+        if (!GetComponent<Renderer>().isVisible)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
---
+++ Assets/Script/comEnemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class comEnemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D collision){
+        //プレイヤーの弾に当たると消える。
+        if (collision.gameObject.tag == "PB"){
+            Destroy(this.gameObject);
+
+        }
+
+
+    }
+}
